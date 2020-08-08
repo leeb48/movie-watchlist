c7c6e4e6c93581ac41e66a91c3002e1a43cd87ea
Bong Lee <bongster228@github.com>
2020-08-07 22:05:07 -0700

    connected to postgres

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfig } from './db-config/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ api/src/db-config/typeorm.config.ts
@@ -0,0 +1,19 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+// SYNC env variable needs to be converted to a boolean value
+// when it is being used to configure TypeORM
+const sync = (syncOption: string): boolean => {
+  if (syncOption === 'true') return true;
+  else return false;
+};
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: sync(process.env.SYNC),
+};
